093d4fad1fcf8bbc67ed3b0481b3733925830841
Ankur Chaudhary <ankurchaudhary627@gmail.com>
2019-12-02 16:04:51 +0530

    Add files via upload

--- Day1.js
+++ Day1.js
@@ -1,11 +1,34 @@
 let fuelData = require('./fuelData.json');
 
-function calcFuel(){
-    var reqdFuel=0;
-    for(var idx in fuelData){
-        reqdFuel+=Math.floor(Number(fuelData[idx])/3)-2;
-    }
-    return reqdFuel;
+function calcFuel(fuel){
+    var value = Math.floor(Number(fuel)/3)-2;
+    return value > 0 ? value : 0;
 }
 
-console.log(calcFuel());
+function calcRecursiveFuel(){
+    
+    var fuelArray = [];
+
+    for(var idx in fuelData){
+
+        var reqdFuel=0;
+        var currFuel=fuelData[idx];
+        
+        while(currFuel>0){
+            currFuel=calcFuel(currFuel);
+            reqdFuel+=currFuel;
+        }
+        
+        fuelArray.push(reqdFuel);
+    }
+    return fuelArray.reduce((a,b) => a + b, 0);
+}
+
+var singleFuel=0;
+
+for(var idx in fuelData){
+    singleFuel+=calcFuel(fuelData[idx]);
+}
+console.log(singleFuel);
+
+console.log(calcRecursiveFuel());
